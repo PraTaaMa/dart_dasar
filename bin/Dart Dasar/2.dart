ca0d60be3f9fd478fd1be2aa4ea5d64dd6c03be7
PraTaaMa <rockypratama4@gmail.com>
2025-09-17 05:24:57 +0700

    Operator

---
+++ bin/Dart Dasar/2.dart
@@ -0,0 +1,18 @@
+//Operator
+
+//Menghitung total belanja
+
+void main(){
+  num duitnyaIbu = 200000;
+  num ayam = 50000;
+  num ikan = 20000;
+  num telor = 2000;
+
+  num totalTelor = telor * 10;
+  num totalBelanja = ayam + ikan;
+  num total1 = totalTelor + totalBelanja;
+  num sisa = duitnyaIbu - total1;
+
+  print(sisa);    
+
+}
